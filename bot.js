c847e0357f4163cc390af303b42674f6504c0a1a
Alan Leal <alanmeira@gmail.com>
2017-12-31 10:27:51 -0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,42 @@
+var env = require('node-env-file');
+env(__dirname + '/.env');
+
+
+if (!process.env.page_token) {
+    console.log('Error: Specify a Facebook page_token in environment.');
+    process.exit(1);
+}
+
+if (!process.env.verify_token) {
+    console.log('Error: Specify a Facebook verify_token in environment.');
+    process.exit(1);
+}
+
+var Botkit = require('botkit');
+var debug = require('debug')('botkit:main');
+
+// Create the Botkit controller, which controls all instances of the bot.
+var controller = Botkit.facebookbot({
+    debug: true,
+    verify_token: process.env.verify_token,
+    access_token: process.env.page_token,
+    studio_token: process.env.studio_token,
+    studio_command_uri: process.env.studio_command_uri,
+});
+
+// Set up an Express-powered webserver to expose oauth and webhook endpoints
+var webserver = require(__dirname + '/components/express_webserver.js')(controller);
+
+// Tell Facebook to start sending events to this application
+require(__dirname + '/components/subscribe_events.js')(controller);
+
+if (process.env.studio_token) {
+    controller.on('message_received,facebook_postback', function(bot, message) {
+        console.log(message.text.toLowerCase());
+        if (message.text.toLowerCase() === "teste") {
+            console.log("funcionou 2");
+            bot.reply(message, "Espere um pouco...");
+            bot.reply(message, "Ok! Testado.");
+        }
+    });
+}
